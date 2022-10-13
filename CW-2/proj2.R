62e4711d9815fed2a2b86c45eff27c115a87d9b0
Alannahyh <s2434943@ed.ac.uk>
2022-10-13 17:26:27 +0100

    Q1

--- CW-2/proj2.R
+++ CW-2/proj2.R
@@ -1,7 +1,7 @@
 # Karman Singh s1936373
 # Viren Sirwani Mulani s1949143
 # Alannah Hounat s2434943
-
+# Git repo Link: 
 # Team member contributions to project:
 
 Pone <- function(n, k, strategy, nreps) {
@@ -26,10 +26,37 @@ Pone <- function(n, k, strategy, nreps) {
         }
       }
     }
+    
     else if (strategy == 2) {
+      guess<-sample(1:n,1)
+      while(number_of_attempts<=n){
+        if(guess==k){
+          number_of_success <- number_of_success + 1
+          break
+        }
+        else{
+          guess<-numbered_boxes[guess]
+          number_of_attempts <- number_of_attempts + 1
+        }
+      }
       
     }
     else {
+      #first we want to pick a box at random
+      random_box<-sample(1:n,n)
+      while(number_of_attemps<=n){
+        #if this random box contains the prisoners number
+        if(random_box=k){
+          #count successes
+          number_of_success<- number_of_success + 1
+        }
+        else{
+          #pick a new random box
+          random_box<-sample(1:n,n)
+          #count fails
+          number_of_attempts<-number_of_attempts +1
+        }
+      }
       
     }
     
