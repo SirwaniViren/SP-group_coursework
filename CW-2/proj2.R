b84a732952fc721b1b3619e6fe95172fd5c4f497
Viren Sirwani Mulani <sirwani.viren@gmail.com>
2022-10-13 17:46:01 +0100

    helper function to make sure we dont repear code

--- CW-2/proj2.R
+++ CW-2/proj2.R
@@ -4,6 +4,23 @@
 # Git repo Link: 
 # Team member contributions to project:
 
+success_check <- function(n, k, try){
+  number_of_attempts <- 0
+  number_of_success <- 0
+  while (number_of_attempts <= n) {
+    if (try == k) {
+      number_of_success <- number_of_success + 1
+      break
+    }
+    else {
+      try <- numbered_boxes[try]
+      number_of_attempts <- number_of_attempts + 1
+    }
+  }
+  
+  return (c(number_of_attempts, number_of_success))
+}
+
 Pone <- function(n, k, strategy, nreps) {
   number_of_success <- 0
   for (reps in 1:nreps) {
@@ -14,36 +31,23 @@ Pone <- function(n, k, strategy, nreps) {
     number_of_attempts <- 0
     
     if (strategy == 1) {
-      try <- numbered_boxes[k]
-      while (number_of_attempts <= n) {
-        if (try == k) {
-          number_of_success <- number_of_success + 1
-          break
-        }
-        else {
-          try <- numbered_boxes[try]
-          number_of_attempts <- number_of_attempts + 1
-        }
-      }
+      guess <- numbered_boxes[k]
+      check <- success_check(n, k,guess)
+      number_of_attempts <- check[[1]]
+      number_of_success <- check[[2]]
     }
     
     else if (strategy == 2) {
-      guess<-sample(1:n,1)
-      while(number_of_attempts<=n){
-        if(guess==k){
-          number_of_success <- number_of_success + 1
-          break
-        }
-        else{
-          guess<-numbered_boxes[guess]
-          number_of_attempts <- number_of_attempts + 1
-        }
+      guess<-sample(1:(2*n),1)
+      check <- success_check(n, k, guess)
+      number_of_attempts <- check[1]
+      number_of_success <- check[2]
       }
       
     }
     else {
       #first we want to pick a box at random
-      random_box<-sample(1:n,n)
+      random_box<-sample(1:(2*n),n)
       while(number_of_attemps<=n){
         #if this random box contains the prisoners number
         if(random_box=k){
