d3d31614e4a1fdb0d4f0ce35afd8889e0fef4730
Karman Singh <s1936373@ed.ac.uk>
2022-11-17 20:54:34 +0000

    all done

--- CW-4/proj4.r
+++ CW-4/proj4.r
@@ -195,17 +195,3 @@ newt <- function(theta, func, grad, hess = NULL,..., tol = 1e-8, fscale = 1,
   
   return(return_vals)
 }
-
-rb1 <- function(th,k=2) {
-  k*(th[2]-th[1]^2)^2 + (1-th[1])^2
-}
-gb1 <- function(th,k=2) {
-  c(-2*(1-th[1])-k*4*th[1]*(th[2]-th[1]^2),k*2*(th[2]-th[1]^2))
-}
-hb1 <- function(th,k=2) {
-  h <- matrix(0,2,2)
-  h[1,1] <- 2-k*2*(2*(th[2]-th[1]^2) - 4*th[1]^2)
-  h[2,2] <- 2*k
-  h[1,2] <- h[2,1] <- -4*k*th[1]
-  h
-}
